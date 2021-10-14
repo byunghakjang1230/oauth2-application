ea4594ec1da9a304d9174e5cb96e53249cf89cdb
byunghakjang1230 <byunghakjang1230@gmail.com>
2021-10-14 09:11:47 +0900

    * chore: 잘못 설정된 멀티모듈 삭제

--- m_client-authorization-code/src/main/java/kr/co/kumsung/clientauthorizationcode/domain/ClientUserRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.kumsung.clientauthorizationcode.domain;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientUserRepository extends JpaRepository<ClientUser, Long> {
-    Optional<ClientUser> findByUserName(String userName);
-}
