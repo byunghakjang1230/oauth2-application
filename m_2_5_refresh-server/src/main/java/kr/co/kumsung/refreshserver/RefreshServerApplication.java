5f8f5337c91cb3ca838c7391b08d9b63f14b543d
byunghakjang1230 <byunghakjang1230@gmail.com>
2021-10-15 02:26:46 +0900

    * feat: Create RefreshServer Module
    * docs: Create RefreshServer README.md
    
    * chore: Include RefreshServer Module

---
+++ m_2_5_refresh-server/src/main/java/kr/co/kumsung/refreshserver/RefreshServerApplication.java
@@ -0,0 +1,11 @@
+package kr.co.kumsung.refreshserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RefreshServerApplication.class, args);
+    }
+}
